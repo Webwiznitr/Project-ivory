1db1b98059a51dd5a03de121942975dcd0188ee4
deb2000-sudo <59384249+deb2000-sudo@users.noreply.github.com>
2020-12-03 14:50:46 +0530

    Add files via upload

--- project_poster/index.php
+++ project_poster/index.php
@@ -8,28 +8,56 @@ if(isset($_POST['posttitle'])){
     imagettftext($image,40,0,450,840,$color,$font,$_POST['authorname']);
     $date=$_POST['authorname'];
     
+    
+    $extensions= array("jpeg","jpg","png");
+    
     // Upload Signature
     if(isset($_FILES['Signature'])){
         $errors= array();
-        $file_name = $_FILES['Signature']['name'];
-        $file_size =$_FILES['Signature']['size'];
-        $file_tmp =$_FILES['Signature']['tmp_name'];
-        $file_type=$_FILES['Signature']['type'];
-        $tmp = explode('.',$_FILES['Signature']['name']);
-        $file_ext=strtolower(end($tmp));
+        $signature_file_name = $_FILES['Signature']['name'];
+        $signature_file_size =$_FILES['Signature']['size'];
+        $signature_file_tmp =$_FILES['Signature']['tmp_name'];
+        $signature_file_type=$_FILES['Signature']['type'];
+        $signature_tmp = explode('.',$_FILES['Signature']['name']);
+        $signature_file_ext=strtolower(end($signature_tmp));
         
-        $extensions= array("jpeg","jpg","png");
         
-        if(in_array($file_ext,$extensions)=== false){
+        if(in_array($signature_file_ext,$extensions)=== false){
             $errors[]="extension not allowed, please choose a JPEG or PNG file.";
         }
         
-        if($file_size > 2097152){
+        if($signature_file_size > 2097152){
             $errors[]='File size must be exactely 2 MB';
         }
         
         if(empty($errors)==true){
-            move_uploaded_file($file_tmp,"./uploads/".$file_name);
+            move_uploaded_file($signature_file_tmp,"./uploads/".$signature_file_name);
+        }else{
+            print_r($errors);
+        }
+    }
+    
+    // Upload Author's Image
+    if(isset($_FILES['AuthorImage'])){
+        $errors= array();
+        $authorImage_file_name = $_FILES['AuthorImage']['name'];
+        $authorImage_file_size =$_FILES['AuthorImage']['size'];
+        $authorImage_file_tmp =$_FILES['AuthorImage']['tmp_name'];
+        $authorImage_file_type=$_FILES['AuthorImage']['type'];
+        $authorImage_tmp = explode('.',$_FILES['AuthorImage']['name']);
+        $authorImage_file_ext=strtolower(end($authorImage_tmp));
+        
+        
+        if(in_array($authorImage_file_ext,$extensions)=== false){
+            $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+        }
+        
+        if($authorImage_file_size > 2097152){
+            $errors[]='File size must be exactely 2 MB';
+        }
+        
+        if(empty($errors)==true){
+            move_uploaded_file($authorImage_file_tmp,"./uploads/".$authorImage_file_name);
             echo "<script>alert('Success')</script>";
         }else{
             print_r($errors);
@@ -37,15 +65,38 @@ if(isset($_POST['posttitle'])){
     }
     
     // Merge Images
-    $src = imagecreatefromjpeg("./uploads/".$file_name);
+    $signature_src = imagecreatefromjpeg("./uploads/".$signature_file_name);
+    $authorImage_src = imagecreatefromjpeg("./uploads/".$authorImage_file_name);
     
+    // Make it Circular    
+    /*
+    $x=imagesx($authorImage_src)-100 ;
+    $y=imagesy($authorImage_src)-100;    
+    
+    $img2 = imagecreatetruecolor($x, $y);
+    $bg = imagecolorallocate($img2, 255, 255, 255); 
+    imagefill($img2, 0, 0, $bg);
+    
+    $e = imagecolorallocate($img2, 0, 0, 0); 
+    
+    $r = $x <= $y ? $x : $y; 
+    imagefilledellipse ($img2, ($x/2), ($y/2), $r, $r, $e); 
+    
+    imagecolortransparent($img2, $e);
+    
+    imagecopymerge($authorImage_src, $img2, 0, 0, 0, 0, $x, $y, 100);
+    
+    imagecolortransparent($authorImage_src, $bg);
+    */
     imagealphablending($image, false);
     imagesavealpha($image, true);
     
-    imagecopymerge($image, $src, 144, 440, 50, 50, 300, 210, 100); //have to play with these numbers for it to work for you, etc.
-
-    imagedestroy($src);
-
+    imagecopymerge($image, $signature_src, 144, 440, 50, 50, 300, 210, 100);
+    imagecopymerge($image, $authorImage_src, 280, 790, 50, 50, 100, 100, 100);
+    
+    imagedestroy($signature_src);
+    imagedestroy($authorImage_src);
+    
     $file=$_POST['posttitle'].time();
     imagejpeg($image,"certificate/".$file.".jpg");
     imagedestroy($image);
@@ -58,7 +109,7 @@ if(isset($_POST['posttitle'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>poster generatorr</title>
+<title>Poster Generator</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/main.css">
 
@@ -79,37 +130,26 @@ if(isset($_POST['posttitle'])){
 <p>Create poster in a second</p>
 </div>
 <div class="row">
-<div class="column_input">
-<form method="post" action="index.php" enctype="multipart/form-data">
-<!--                Enter name-->
-<label for="posttitle">Title of poster</label>
-<input type="text" id="posttitle" name="posttitle" placeholder="Title of post">
-<!--                Enter Event Name-->
-<label for="signature">Upload Post Image</label>
-<input type="file" name="Signature" id="Signature" placeholder="Upload Post Image">
-<br><br>
-<div class="form-group">
-<label for="Post"><span class="fieldinfo">Post description:</span></label>
-<input type="text" id="postdescription" name="postdescription" placeholder="Description about post">
+   <div class="column_input">
+        <form method="post" action="index.php" enctype="multipart/form-data">
+            <label for="posttitle">Title of poster</label>
+            <input type="text" id="posttitle" name="posttitle" placeholder="Title of post">
+            <label for="signature">Upload Post Image</label>
+            <input type="file" name="Signature" id="Signature" placeholder="Upload Post Image">
+                     <br><br>
+            <label for="Post"><span class="fieldinfo">Post description:</span></label>
+            <input type="text" id="postdescription" name="postdescription" placeholder="Description about post">
+            <label for="authorname">Author Name</label>
+            <input type="text" id="authorname" name="authorname" placeholder="author name">
+            <label for="AuthorImage">Upload Author's Image</label>
+            <input type="file" name="AuthorImage" id="AuthorImage" placeholder="Upload Author's Image">
+            <input type="submit" value="Generate poster">
+        </form>
+  </div>
 </div>
-<label for="authorname">Author Name</label>
-<input type="text" id="authorname" name="authorname" placeholder="author name">
-<!--                Enter date of issue-->
-
-<br><br>
-
-
-</div>
-<br><br>
-<input type="submit" value="Generate poster">
-</form>
-</div>
-<br><br>
+                      <br><br>
 <div class="column_output">
-<img src="templates/temp_1.jpg" alt="image" style="width:70%">
-</div>
-
-</div>
+     <img src="templates/temp_1.jpg" alt="image" style="width:70%">
 </div>
 
 </body>
